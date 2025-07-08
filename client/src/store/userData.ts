af3f66f375fe3947e498967bf7f406644db24556
Moheeb <moheebk522@gmail.com>
2025-07-08 22:32:10 +0530

    refactor(store): remove theme slice and clean up userData slice formatting

--- client/src/store/userData.ts
+++ client/src/store/userData.ts
@@ -1,15 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const userDataSlice = createSlice({
-    name: "userData",
-    initialState: {
-        isCheckingAuth: true,
-        isAuthenticated: false,
-    },
-    reducers: {
-        updateUser: (state, action) =>
-            (state = action.payload ? { ...action.payload, isCheckingAuth: false, isAuthenticated: true } : { isCheckingAuth: false, isAuthenticated: false }),
-    },
+  name: "userData",
+  initialState: {
+    isCheckingAuth: true,
+    isAuthenticated: false,
+  },
+  reducers: {
+    updateUser: (state, action) =>
+      (state = action.payload
+        ? { ...action.payload, isCheckingAuth: false, isAuthenticated: true }
+        : { isCheckingAuth: false, isAuthenticated: false }),
+  },
 });
 
 export const userDataActions = userDataSlice.actions;
