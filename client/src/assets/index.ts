5face7ceb7fe60f51d4cc2ae227a1a455d5b70cf
Moheeb <moheebk522@gmail.com>
2025-07-08 13:44:54 +0530

    feat(assets): add image in assets

---
+++ client/src/assets/index.ts
@@ -0,0 +1,7 @@
+import logo from "./logo.png";
+import calender from "./calendar.png";
+import show from "./show.png";
+import hide from "./hide.png";
+import bin from "./delete.png";
+
+export { logo, calender, show, hide, bin };
