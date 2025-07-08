a9de618a236c99bc4d2834032f33ed4f69c613a5
Moheeb <moheebk522@gmail.com>
2025-07-08 22:31:53 +0530

    refactor(routes): clean up ProtectedRoute component and remove propTypes

--- client/src/routes/ProtectedRoute.tsx
+++ client/src/routes/ProtectedRoute.tsx
@@ -1,22 +1,19 @@
 import { Navigate, Outlet } from "react-router-dom";
-import propTypes from "prop-types";
 
 interface ProtectedPropsType {
   isAuthenticated: boolean;
   isUserAllowed: boolean;
 }
 
-const ProtectedRoute = ({ isAuthenticated, isUserAllowed } : ProtectedPropsType) => {
+const ProtectedRoute = ({
+  isAuthenticated,
+  isUserAllowed,
+}: ProtectedPropsType) => {
   if (isAuthenticated) {
     return isUserAllowed ? <Outlet /> : <Navigate to="/not-allowed" />;
   } else {
     return isUserAllowed ? <Navigate to="/auth/login" /> : <Outlet />;
   }
-};
-
-ProtectedRoute.propTypes = {
-  isUserAllowed: propTypes.bool.isRequired,
-  isAuthenticated: propTypes.bool.isRequired,
 };
 
 export default ProtectedRoute;
