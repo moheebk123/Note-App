fa02cdfb7d12480a85789929e66ae044b344e72d
Moheeb <moheebk522@gmail.com>
2025-07-08 22:31:35 +0530

    feat(assets): add new edit, sign-in, and sign-up images; update asset exports

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -3,6 +3,9 @@ import calender from "./calendar.png";
 import show from "./show.png";
 import hide from "./hide.png";
 import bin from "./delete.png";
+import edit from "./edit.png"
 import loader from "./loader.svg";
+import signIn from "./sign-in.png";
+import signUp from "./sign-up.png";
 
-export { logo, calender, show, hide, bin, loader };
+export { logo, calender, show, hide, bin, edit, loader, signIn, signUp };
