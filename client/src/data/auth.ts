fd3533f295b67481c6a4448be42f26c67f8fc97c
Moheeb <moheebk522@gmail.com>
2025-07-08 13:48:27 +0530

    define data for not found and not allowed page

---
+++ client/src/data/auth.ts
@@ -0,0 +1,9 @@
+export const notFoundText = {
+  primaryText: "404",
+  secondaryText: "The page you're looking for doesn't exist..",
+};
+
+export const notAllowedText = {
+  primaryText: "Access Denied",
+  secondaryText: "You are not allowed to visit this page.",
+};
